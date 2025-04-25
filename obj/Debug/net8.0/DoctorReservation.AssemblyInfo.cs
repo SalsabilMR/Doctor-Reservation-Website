5adb1c89a9aa811c2114d167e41ae216481602bb
teresa-girgis <tawfik.teresa@gmail.com>
2025-04-25 13:56:09 +0300

    new clone

--- obj/Debug/net8.0/DoctorReservation.AssemblyInfo.cs
+++ obj/Debug/net8.0/DoctorReservation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoctorReservation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b23bb9b27202d2474865e4fa251023ab868a1dbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a0cd47dfa9b1dc07baf2ecae37fed5c68c0ea6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoctorReservation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoctorReservation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
