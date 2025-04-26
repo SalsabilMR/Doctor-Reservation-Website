19f1a7b4c751bb0835f721eb4dafe7ad5c664057
teresa-girgis <tawfik.teresa@gmail.com>
2025-04-26 21:10:40 +0300

    add validation & add libraries to work server side

--- Models/Doctor.cs
+++ Models/Doctor.cs
@@ -7,16 +7,22 @@ namespace DoctorReservation.Models
     {
         public int Id { get; set; }
         [Required(ErrorMessage ="Please enter your first name")]
+        [StringLength(20, ErrorMessage = "Name not more than 20 letters")]
+        [MinLength(3, ErrorMessage = "Name not less than 3 letters")]
         public string? FirstName { get; set; }
         [Required(ErrorMessage = "Please enter your last name")]
+        [StringLength(20, ErrorMessage = "Name not more than 20 letters")]
+        [MinLength(3, ErrorMessage = "Name not less than 3 letters")]
         public string? LastName { get; set; }
         [Required(ErrorMessage = "Please enter your city")]
         public string? City { get; set; }
         [Required(ErrorMessage = "Please enter your location")]
         public string? Location { get; set; }
         [Required(ErrorMessage = "Please enter your email")]
+        [EmailAddress(ErrorMessage = "Invalid email")]
         public string? Email { get; set; }
         [Required(ErrorMessage = "Please enter your Mobile")]
+        [Phone(ErrorMessage = "Invalid Number")]
         public int Mobile { get; set; }
         [Required(ErrorMessage = "Please enter your fees")]
         public int Fees { get; set; }
@@ -25,12 +31,12 @@ namespace DoctorReservation.Models
         [Required(ErrorMessage = "Please enter your Description")]
         public string? Description { get; set; }
 
-        //[Required(ErrorMessage = "Please enter your Certificate")]
+        [Required(ErrorMessage = "Please enter your Certificate")]
         public string? CertificatePath { get; set; } //save file name & file path in DB
         [NotMapped]
         public IFormFile? Certificate { get; set; } //recieve file when upload
 
-       /// [Required(ErrorMessage = "Please enter your Image")]
+        [Required(ErrorMessage = "Please enter your Image")]
         public string? ImagePath { get; set; } //save file name in DB
         [NotMapped]
         public IFormFile? Image { get; set; }
--- Models/Patient.cs
+++ Models/Patient.cs
@@ -1,14 +1,26 @@
-﻿using System.Runtime.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DoctorReservation.Models
 {
     public class Patient
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter your first name")]
+        [StringLength( 20, ErrorMessage = "Name not more than 20 letters")]
+        [MinLength(3, ErrorMessage = "Name not less than 3 letters")]
         public string? FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter your last name")]
+        [StringLength(20, ErrorMessage = "Name not more than 20 letters")]
+        [MinLength(3, ErrorMessage = "Name not less than 3 letters")]
         public string? LastName { get; set; }
+        [Required(ErrorMessage = "Please enter your gender")]
         public Gender _Gender { get; set; }
+        [Required(ErrorMessage = "Please enter your first Birth of date")]
+        [DataType(DataType.Date)]
         public DateTime BirthOfDate { get; set; }
+        [Required(ErrorMessage = "Please enter your mobile")]
+        [Phone(ErrorMessage = "Invalid Number")]
         public int Mobile { get; set; }
 
         public enum Gender
